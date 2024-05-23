fc121cbac60205380a9f968e981a32746d974cbe
= <tky1245@gmail.com>
2024-05-23 18:42:03 +0800

    removed some unused code

--- main_menu.gd
+++ main_menu.gd
@@ -6,11 +6,6 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_new_chart_pressed():
 	if !$SelectSong.visible:
 		$SelectSong.visible = true
