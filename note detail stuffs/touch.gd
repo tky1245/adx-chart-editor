676e58db5744dd80eeab4f0479878db5041ae9cb
= <tky1245@gmail.com>
2024-06-18 14:59:31 +0800

    redrew touch note

--- note detail stuffs/touch.gd
+++ note detail stuffs/touch.gd
@@ -6,7 +6,7 @@ var note_property_ex: bool
 var note_property_touch: bool
 var note_property_firework: bool
 var note_property_both: bool
-var note_property_slider: bool
+var note_property_star: bool
 var note_position: String
 var sliders: Array = []
 
@@ -38,13 +38,9 @@ func note_render(current_time: float) -> void:
 		scale_progress = 1
 		path_progress = 1
 	
+	set_node_images_transparency($Note, scale_progress)
 	for i in range(5):
 		var note_pathfollow = $Note.get_child(i).get_child(0).get_child(0)
-		
-		for node in note_pathfollow.get_children():
-			#node.scale = Vector2(scale_progress, scale_progress)
-			node.self_modulate.a = scale_progress
-		
 		note_pathfollow.progress_ratio = path_progress
 
 func slider_render(current_time: float) -> void:
@@ -57,40 +53,100 @@ func initialize() -> void:
 		$Note.position = note_pos
 		$Sliders.position = note_pos
 		
-		var note_color
+		# Colors
+		#TODO: redraw touches
+		var note_color_timeline_indicator
+		var note_color_outer
 		var note_color_inner
-		if note_property_break: # um break touches
-			note_color = Global.note_colors["note_break"]
+		var note_color_outline = Color.BLACK
+		var note_color_highlight_ex
+		
+		#TODO: redo touch stars
+		if note_property_break and note_property_star:
+			note_color_timeline_indicator = Global.note_colors["star_indicator_break"]
+			note_color_outer = Global.note_colors["star_outer_break"]
+			note_color_inner = Global.note_colors["star_inner_break"]
+			note_color_highlight_ex = Global.note_colors["star_highlight_ex_break"]
+		elif note_property_break:
+			note_color_timeline_indicator = Global.note_colors["touch_indicator_break"]
+			note_color_outer = Global.note_colors["touch_outer_break"]
+			note_color_inner = Global.note_colors["touch_inner_break"]
+			note_color_highlight_ex = Global.note_colors["touch_highlight_ex_break"]
+		elif note_property_both and note_property_star:
+			note_color_timeline_indicator = Global.note_colors["star_indicator_both"]
+			note_color_outer = Global.note_colors["star_outer_both"]
+			note_color_inner = Global.note_colors["star_inner_both"]
+			note_color_highlight_ex = Global.note_colors["star_highlight_ex_both"]
 		elif note_property_both:
-			note_color = Global.note_colors["note_both"]
-		elif sliders.size() > 0:
-			note_color = Global.note_colors["star_outer"]
-			note_color_inner = Global.note_colors["star_inner"]
+			note_color_timeline_indicator = Global.note_colors["touch_indicator_both"]
+			note_color_outer = Global.note_colors["touch_outer_both"]
+			note_color_inner = Global.note_colors["touch_inner_both"]
+			note_color_highlight_ex = Global.note_colors["touch_highlight_ex_both"]
+		elif note_property_star:
+			note_color_timeline_indicator = Global.note_colors["star_indicator_base"]
+			note_color_outer = Global.note_colors["star_outer_base"]
+			note_color_inner = Global.note_colors["star_inner_base"]
+			note_color_highlight_ex = Global.note_colors["star_highlight_ex_base"]
 		else:
-			note_color = Global.note_colors["touch_base"]
+			note_color_timeline_indicator = Global.note_colors["touch_indicator_base"]
+			note_color_outer = Global.note_colors["touch_outer_base"]
+			note_color_inner = Global.note_colors["touch_inner_base"]
+			note_color_highlight_ex = Global.note_colors["touch_highlight_ex_base"]
+		
 		
 		for node in $Note/CenterDot.get_children():
 			node.queue_free()
-		if sliders.size() == 0:
-			var note_center_highlight = circle(Color.WHITE, 10, 2, 16)
-			$Note/CenterDot.add_child(note_center_highlight)
-			var note_center_dot = circle(note_color, 6, 2, 16)
+		if !note_property_star:
+			var note_center_outline = circle(Color.BLACK, 7, 2, 16)
+			$Note/CenterDot.add_child(note_center_outline)
+			var note_center_dot = circle(note_color_outer, 5, 2, 16)
 			$Note/CenterDot.add_child(note_center_dot)
 		
-		if sliders.size() == 0: # normal touch
+		if !note_property_star: # normal touch
 			for i in range(4):
 				var note_path = $Note.get_child(i).get_child(0)
 				var note_pathfollow = note_path.get_child(0)
 				for node in note_pathfollow.get_children():
 					node.queue_free()
-				var note_outline = triangle(Color.WHITE, 10)
-				note_pathfollow.add_child(note_outline)
-				var note_triangle_part = triangle(note_color, 6)
-				note_pathfollow.add_child(note_triangle_part)
+				
+				var size = 22
+				var width = 6
+				var segment_outer = Polygon2D.new()
+				segment_outer.polygon = touch_outer(size, width, 2)
+				segment_outer.color = note_color_outer
+				note_pathfollow.add_child(segment_outer)
+				var segment_inner = Polygon2D.new()
+				segment_inner.polygon = touch_inner(size, width, 2)
+				print(segment_inner.polygon)
+				segment_inner.color = note_color_inner
+				print(note_color_inner)
+				note_pathfollow.add_child(segment_inner)
+				var note_outline_in = Line2D.new()
+				for point in triangle(size-width*(1+2*sin(TAU/8)), 2+width*2*sin(TAU/8)):
+					note_outline_in.add_point(point)
+				note_outline_in.default_color = Color.WHITE
+				note_outline_in.closed = true
+				note_outline_in.width = 1
+				note_pathfollow.add_child(note_outline_in)
+				var note_outline_out = Line2D.new()
+				for point in touch(size, 2):
+					note_outline_out.add_point(point)
+				note_outline_out.default_color = Color.WHITE
+				note_outline_out.closed = true
+				note_outline_out.width = 1
+				note_pathfollow.add_child(note_outline_out)
+				var note_outline_out_2 = Line2D.new()
+				for point in touch(size+4, 0.5):
+					note_outline_out_2.add_point(point)
+				note_outline_out_2.default_color = Color.BLACK
+				note_outline_out_2.closed = true
+				note_outline_out_2.width = 1
+				note_pathfollow.add_child(note_outline_out_2)
+				
 				var offset = 12
 				var new_curve = Curve2D.new()
-				new_curve.add_point(Vector2(9 + offset, 0).rotated(TAU * i / 4))
-				new_curve.add_point(Vector2(9, 0).rotated(TAU * i / 4))
+				new_curve.add_point(Vector2(6 + offset, 0).rotated(TAU * i / 4))
+				new_curve.add_point(Vector2(0, 0))
 				note_path.curve = new_curve
 			for node in $Note/Segment4/Path2D/PathFollow2D.get_children():
 				node.queue_free()
@@ -102,7 +158,7 @@ func initialize() -> void:
 					node.queue_free()
 				var note_polygon = Polygon2D.new()
 				note_polygon.polygon = star_rhombus()
-				note_polygon.color = note_color
+				note_polygon.color = note_color_outer
 				note_polygon.rotation = PI
 				note_pathfollow.add_child(note_polygon)
 				var outline = Line2D.new()
@@ -128,7 +184,7 @@ func initialize() -> void:
 		for node in $TimelineIndicator.get_children():
 			node.free()
 		var new_line = Line2D.new()
-		new_line.default_color = note_color
+		new_line.default_color = note_color_timeline_indicator
 		new_line.width = 2
 		new_line.add_point(Vector2(4, 4))
 		new_line.add_point(Vector2(4, -4))
@@ -157,24 +213,54 @@ func circle(color: Color, width: float, radius: float = 18, frequency: int = 360
 		newLine.add_point(Vector2(radius * sin(angle), radius * cos(angle)))
 	return newLine
 
-func triangle(color: Color, width: float) -> Line2D:
-	var newLine = Line2D.new()
-	newLine.default_color = color
-	newLine.width = width
-	newLine.closed = true
-	newLine.add_point(Vector2(-2, 0))
-	newLine.add_point(Vector2(-16, 14))
-	newLine.add_point(Vector2(-16, -14))
-	return newLine
+func triangle(size: float, offset: float) -> PackedVector2Array:
+	var polygon: PackedVector2Array = []
+	polygon.append(Vector2(-offset, 0))
+	polygon.append(Vector2(-offset-size, size))
+	polygon.append(Vector2(-offset-size, -size))
+	return polygon
+
+func touch(size: float, offset: float) -> PackedVector2Array:
+	var polygon: PackedVector2Array = []
+	var dy: float = (size - 22)/4.0 * 5 / sqrt(3)
+	var dx: float = (size - 22)/4.0 * sqrt(3)
+	polygon.append(Vector2(-offset, 0))
+	polygon.append(Vector2(-offset-size+3+dx, size-3-dy))
+	polygon.append(Vector2(-offset-size+dx, size-8-dy))
+	polygon.append(Vector2(-offset-size+dx, -size+8+dy))
+	polygon.append(Vector2(-offset-size+3+dx, -size+3+dy))
+	return polygon
+
+func touch_outer(size: float, width: float, offset: float) -> PackedVector2Array:
+	var polygon: PackedVector2Array = []
+	polygon.append(Vector2(-offset-size+width, size-width*(1+2*sin(TAU/8))))
+	polygon.append(Vector2(-offset-size+width*(1+sin(TAU/8)), size-width*(1+sin(TAU/8))))
+	polygon.append(Vector2(-offset-size+4, size-4))
+	polygon.append(Vector2(-offset-size, size-8))
+	polygon.append(Vector2(-offset-size, -size+8))
+	polygon.append(Vector2(-offset-size+4, -size+4))
+	polygon.append(Vector2(-offset-size+width*(1+sin(TAU/8)), -size+width*(1+sin(TAU/8))))
+	polygon.append(Vector2(-offset-size+width, -size+width*(1+2*sin(TAU/8))))
+	return polygon
+
+func touch_inner(size: float, width: float, offset: float) -> PackedVector2Array:
+	var polygon: PackedVector2Array = []
+	polygon.append(Vector2(-offset, 0))
+	polygon.append(Vector2(-offset-size+width*(1+sin(TAU/8)), size-width*(1+sin(TAU/8))))
+	polygon.append(Vector2(-offset-size+width, size-width*(1+2*sin(TAU/8))))
+	polygon.append(Vector2(-offset-2*width*sin(TAU/8), 0))
+	polygon.append(Vector2(-offset-size+width, -size+width*(1+2*sin(TAU/8))))
+	polygon.append(Vector2(-offset-size+width*(1+sin(TAU/8)), -size+width*(1+sin(TAU/8))))
+	return polygon
 
 func star_rhombus(inner_radius: float = 16, outer_radius: float = 26) -> PackedVector2Array: # for touch stars
 	var new_polygon: PackedVector2Array = []
 	new_polygon.append(Vector2(outer_radius - outer_radius * sin(TAU/10)*cos(TAU/20)/sin(TAU*7/20), -outer_radius*sin(TAU/10)*sin(TAU/20)/sin(TAU*7/20)))
 	new_polygon.append(Vector2(outer_radius, 0))
 	new_polygon.append(Vector2(outer_radius - outer_radius * sin(TAU/10)*cos(TAU/20)/sin(TAU*7/20), outer_radius*sin(TAU/10)*sin(TAU/20)/sin(TAU*7/20)))
-	new_polygon.append(Vector2(inner_radius - inner_radius * sin(TAU/10)*cos(TAU/20)/sin(TAU*7/20), -inner_radius*sin(TAU/10)*sin(TAU/20)/sin(TAU*7/20)))
-	new_polygon.append(Vector2(inner_radius, 0))
 	new_polygon.append(Vector2(inner_radius - inner_radius * sin(TAU/10)*cos(TAU/20)/sin(TAU*7/20), inner_radius*sin(TAU/10)*sin(TAU/20)/sin(TAU*7/20)))
+	new_polygon.append(Vector2(inner_radius, 0))
+	new_polygon.append(Vector2(inner_radius - inner_radius * sin(TAU/10)*cos(TAU/20)/sin(TAU*7/20), -inner_radius*sin(TAU/10)*sin(TAU/20)/sin(TAU*7/20)))
 	return new_polygon
 
 func create_slider(slider_args: Dictionary):
@@ -191,3 +277,10 @@ func create_slider(slider_args: Dictionary):
 func delete_slider(slider_index: int):
 	$Sliders.get_child(slider_index).queue_free()
 	sliders.pop_at(slider_index)
+
+func set_node_images_transparency(node: Node2D, transparency: float) -> void:
+	if node.get_children().size() == 0:
+		node.self_modulate.a = transparency
+	else:
+		for child_node in node.get_children():
+			set_node_images_transparency(child_node, transparency)
